0c6874203a333bc80ab6f7cfc0069c1120552fa8
yannickdebree <36421603+yannickdebree@users.noreply.github.com>
2022-09-20 13:53:43 +0200

    Draft

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
 // Used in Stackblitz
 
 console.log('Hello world !');
+
+async function main() {
+  const a = await either(async () => {});
+  const a1 = a.onLeft();
+  const a11 = a1.onLeft();
+  const a12 = a1.onRight();
+}
+
+main();
